15d020748fa28fce5bd9b6839a2b07058a9a5b36
Deni Bertovic <deni@kset.org>
2013-05-09 10:05:20 +0200

    modified gitignore, added setup.py and manifest

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+VERSION = '0.1dev'
+
+setup(
+    name='django-client-certificates',
+    version=VERSION,
+    author='Deni Bertovic',
+    author_email='deni@initeq.net',
+    description='Django auth using client certificates',
+    packages=['client_certs'],
+    license='BSD',
+    long_description=open('README.md').read(),
+    install_requires='pyOpenSSL>=0.13',
+)
