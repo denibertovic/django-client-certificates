7c0bacc5db266ddd080b5129f1a06179ed729105
Deni Bertovic <deni@kset.org>
2013-05-09 10:32:43 +0200

    new modifications to setup

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from distutils.core import setup
 
+import os
+import sys
+
 VERSION = '0.1dev'
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
 
 setup(
     name='django-client-certificates',
@@ -8,8 +16,17 @@ setup(
     author='Deni Bertovic',
     author_email='deni@kset.org',
     description='Django auth using client certificates',
+    url='github.com/denibertovic/django-client-certificates',
     packages=['client_certs'],
     license='BSD',
-    long_description=open('README.md').read(),
-    install_requires='pyOpenSSL>=0.13',
+    install_requires=['pyOpenSSL>=0.13', 'django>=1.4'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
