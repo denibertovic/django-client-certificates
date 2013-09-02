9c2536ea954026ea8002da862c49fc1dc3ad6ab1
Deni Bertovic <deni@kset.org>
2013-09-02 23:20:39 +0200

    remove compressor from test_project

--- test_project/django_client_certs/urls.py
+++ test_project/django_client_certs/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from client_certs import urls as client_cert_urls
 
@@ -10,3 +11,13 @@ urlpatterns = patterns('',
     url(r'^certs/', include(client_cert_urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+## STATIC AND MEDIA
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}),
+    )
